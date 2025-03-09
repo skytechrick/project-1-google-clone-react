ed30427d8e8f3adfa8e883c6448f9f27268ee4e2
= <ricksarkar2005@gmail.com>
2025-03-10 02:18:24 +0530

    Header done.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Header from './components/header.jsx';
 
 function App() {
     return (
         <>
+            <Header />
         </>
     );
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,32 @@
+import "../css/header.css";
+import rickImg from "../img/rickImg.jpg";
+import appsIconHead from "../img/appsIconHead.png";
+import filterImg from "../img/filterImg.svg";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="head-left">
+                <a href="/about">About</a>
+                <a href="/store">Store</a>
+            </div>
+            <div className="head-right">
+                <div className="mail-image">
+                    <a href="/" style={{marginRight: "5px"}}>Gmail</a>
+                    <a href="/">Images</a>
+                </div>
+                <div className="head-icon-div filter-head">
+                    <img src={filterImg} alt="Filter icon"/>
+                </div>
+                <div className="head-icon-div apps-head">
+                    <img src={appsIconHead} alt="Apps icon" className="head-icon" />
+                </div>
+                <div className="profile-logo">
+                    <img src={rickImg} alt="Profile logo" />
+                </div>
+
+            </div>
+            
+        </header>
+    );
+};
