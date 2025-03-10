1715e9e1e5e303de9c10d138617b85ce1d5a9559
= <ricksarkar2005@gmail.com>
2025-03-11 02:07:43 +0530

    search box completed

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,6 +1,10 @@
 import "../css/body.css";
 
 import googleTextBigImage from "../img/googleTextBigImage.png";
+import searchIcon from "../img/searchIcon.svg";
+import searchClose from "../img/close.svg";
+import micIcon from "../img/micIcon.svg";
+import cameraPhotoIcon from "../img/cameraPhotoIcon.svg";
 
 export default function Body() {
     return (
@@ -9,7 +13,29 @@ export default function Body() {
                 <img src={googleTextBigImage} alt="Google big image" />
             </div>
 
-            <div>
+            <div className="search-bar">
+                <div className="search-bar-container">
+                    <div className="search-icon">
+                        <img src={searchIcon} alt="Search icon" />
+                    </div>
+                    <div className="search-input">
+                        <input autoFocus type="text" placeholder=" " autoComplete="off" spellCheck="false"  />
+                    </div>
+
+                    <div className="search-close">
+                        <img src={searchClose} alt="close image" />
+                    </div>
+
+                    <div className="search-mic">
+                        <img src={micIcon} alt="Mic icon" />
+                    </div>
+
+                    <div className="search-camera-photo">
+                        <img src={cameraPhotoIcon} alt="Camera photo" />
+                    </div>
+
+
+                </div>
 
             </div>
 
